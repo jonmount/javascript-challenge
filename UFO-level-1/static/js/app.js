b40846b8ae37a21fa97262bb4d716bb99463302d
Jonathan Mount <jonmount@mdscom.com>
2021-03-28 10:52:38 -0700

    adding data to HTML table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,26 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//make sure date looks okay in console
+console.log(tableData)
+
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+//loop through each sighting 
+tableData.forEach((sighting) => {
+
+    //append a row for each sighting 
+    var row = tbody.append("tr");
+
+    //use object.entries to append a cell for each key value in each object 
+    Object.entries(sighting).forEach(([key, value]) => {
+  
+      var cell = row.append("td");
+      
+      //append the key value to the table cell 
+      cell.text(value);
+    });
+  });
+  
